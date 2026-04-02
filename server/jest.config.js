e9dc4b03601207c3dfa0a1ffcf40c121bd7918b2
dti3n <nguyentin148@gmail.com>
2026-04-02 11:56:00 +0700

    chore: update jest config & test cmd

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     preset: '@shelf/jest-mongodb',
-    testMatch: ['**/tests/controllers/**/*.test.js', '**/tests/controllers/*.test.js', '**/tests/services/*.test.js'],
+    testMatch: [
+        '**/tests/controllers/card/*.test.js',
+        // '**/tests/controllers/*.test.js',
+        // '**/tests/services/*.test.js'
+    ],
     testTimeout: 20000,
     coveragePathIgnorePatterns: [
         "/lib/",
