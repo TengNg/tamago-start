47765fe7793f5adf397f2c68b2e52f24d12fd65d
dti3n <nguyentin148@gmail.com>
2026-04-08 00:50:09 +0700

    refactor(utils): migrate to esm

--- server/utils/generateRandomHex.js
+++ server/utils/generateRandomHex.js
@@ -1,12 +1,12 @@
-const crypto = require('crypto');
+import { randomBytes } from 'crypto';
 
 /**
  * @param {number} length
  * @returns {string}
  */
 const generateRandomHex = (length) => {
-    return crypto.randomBytes(Math.round(length / 2)).toString('hex');
+    return randomBytes(Math.round(length / 2)).toString('hex');
 };
 
-module.exports = { generateRandomHex };
+export { generateRandomHex };
 
